58b7da5498e8072dbd70f495c545287cc51cf717
dayeongkim999 <dayeongkim999@gmail.com>
2023-04-25 07:02:14 +0900

    first commit

---
+++ src/main/java/com/example/todolist/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @GetMapping("home")
+    public String Home(){
+        return "home";
+    }
+
+
+}
